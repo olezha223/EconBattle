69c20bb33fed2ef85e3cb999e18929fa382dc07b
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-05-07 13:58:14 +0300

    добавил тест на competitions

--- backend/src/repository/competitions.py
+++ backend/src/repository/competitions.py
@@ -7,7 +7,7 @@ from src.repository import RepoInterface
 
 
 class CompetitionsRepo(RepoInterface):
-    async def get_created_competitions(self, user_id: int) -> List[int]:
+    async def get_created_competitions(self, user_id: str) -> List[int]:
         stmt = select(Competition.id).where(Competition.creator_id == user_id)
         async with self.session_getter() as session:
             result = await session.execute(stmt)
--- backend/tests/test_repository/test_competitions.py
+++ backend/tests/test_repository/test_competitions.py
@@ -28,3 +28,14 @@ async def test_get(create_tasks, competition_repo, competition_1_dto, competitio
     dto_2 = await competition_repo.get(object_id=dto_id_2, orm_class=Competition, model_class=CompetitionDTO)
     assert dto_1 == competition_1_dto
     assert dto_2 == competition_2_dto
+
+
+async def test_get_created_competitions(create_tasks, competition_repo, competition_1_dto, competition_2_dto):
+    dto_id_1 = await competition_repo.create(model=competition_1_dto, orm=Competition)
+    dto_id_2 = await competition_repo.create(model=competition_2_dto, orm=Competition)
+    assert dto_id_1 == 1
+    assert dto_id_2 == 2
+
+    assert await competition_repo.get_created_competitions('1') == [1]
+    assert await competition_repo.get_created_competitions('2') == [2]
+    assert await competition_repo.get_created_competitions('3') == []
