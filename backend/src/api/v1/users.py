e473c6e443b777dd72b11c7396f6760b6d7a36a1
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-05-08 19:04:34 +0300

    добавил 404 в юзеров

--- backend/src/api/v1/users.py
+++ backend/src/api/v1/users.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException
+from starlette import status
 
 from src.models.users import UserInfo
 from src.service import UserService, get_user_service
@@ -17,6 +18,12 @@ async def get_user_info(
         user_id: str = Query(..., title="User ID"),
         service: UserService = Depends(get_user_service),
 ) -> UserInfo:
+    user = await service.get_user(user_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
     return await service.get_user_info(user_id)
 
 @router_user.put(
@@ -29,4 +36,10 @@ async def update_username(
         username: str = Query(..., title="New username"),
         service: UserService = Depends(get_user_service),
 ) -> None:
+    user = await service.get_user(user_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
     return await service.update_username(user_id, username)
