58380058fd6480a93f550b701b1e1e9616c3e090
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-05-08 18:53:11 +0300

    добавил ошибку в games

--- backend/src/api/v1/games.py
+++ backend/src/api/v1/games.py
@@ -1,10 +1,10 @@
 from typing import List
 
-from fastapi import APIRouter, Query, Depends
+from fastapi import APIRouter, Query, Depends, HTTPException
 from starlette import status
 
 from src.models.game import GameDTOExtended
-from src.service import GameService, get_game_service
+from src.service import GameService, get_game_service, UserService, get_user_service
 
 router_games = APIRouter(
     prefix="/games",
@@ -22,5 +22,9 @@ router_games = APIRouter(
 async def get_all_games(
         user_id: str = Query(..., description="User ID"),
         service: GameService = Depends(get_game_service),
+        user_service: UserService = Depends(get_user_service)
 ) -> List[GameDTOExtended]:
+    user = await user_service.get_user(user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return await service.get_all(user_id)
