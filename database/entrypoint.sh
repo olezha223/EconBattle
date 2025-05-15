a048869d2d97430a126f5447b8d67c1e8591584e
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-05-15 17:12:40 +0300

    поправил билд бд

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 set -e
 
-# Устанавливаем порт по умолчанию, если он не задан
 PGPORT=${PGPORT:-5432}
-
-# Экспортируем переменные окружения для использования в конфигурации
 export PGPORT
 
-# Если файл /init.sql существует и база данных не инициализирована, выполняем его
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
   echo "Инициализация базы данных..."
 
-  # Инициализация базы данных под пользователем postgres
+  # Запускаем стандартную инициализацию
   gosu postgres /usr/local/bin/docker-entrypoint.sh postgres &
 
-  # Ожидание готовности сервера
+  # Ждем запуска сервера
   until gosu postgres pg_isready -h localhost -p $PGPORT; do
     echo "Ожидание запуска PostgreSQL..."
     sleep 2
   done
 
-  # Замена переменных и выполнение init.sql
-  if [ -f /init.sql ]; then
-    echo "Выполняется /init.sql..."
-    envsubst < /init.sql > /tmp/processed_init.sql
-    chown postgres:postgres /tmp/processed_init.sql
-    gosu postgres psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /tmp/processed_init.sql
-  fi
+  export PGPASSWORD="$POSTGRES_PASSWORD"
+
+  # Создаем базы данных с явным указанием хоста и порта
+  gosu postgres psql -U "$POSTGRES_USER" -h localhost -p $PGPORT -c "CREATE DATABASE \"${DBNAME}\";"
+  gosu postgres psql -U "$POSTGRES_USER" -h localhost -p $PGPORT -c "CREATE DATABASE \"${TEST_DBNAME}\";"
 
   # Остановка сервера
   gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
