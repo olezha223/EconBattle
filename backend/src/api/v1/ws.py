31f65602f74b79da6886a82636564ef2d7a17194
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-05-24 23:08:52 +0300

    Update ws.py

--- backend/src/api/v1/ws.py
+++ backend/src/api/v1/ws.py
@@ -8,7 +8,7 @@ ws_router = APIRouter(
 )
 
 match_maker = MatchMaker()
-
+# single_player_maker = ...
 
 @ws_router.websocket("/")
 async def websocket_endpoint(
@@ -16,11 +16,22 @@ async def websocket_endpoint(
         player_id: str = Query(..., alias="user_id", description="ID of the player"),
         competition_id: int = Query(..., alias="competition_id", description="ID of the competition")
 ):
-    if player_id in match_maker.manager.active_connections.keys():
-        await websocket.accept()
-        await websocket.close()
-        return
     await websocket.accept()
     await websocket.send_json({"type": "connected", "msg": f"Игрок {player_id} подключен к серверу"})
     await match_maker.add_player(websocket, player_id, competition_id)
     print("вышло из игры для игрока", player_id)
+
+#
+# @ws_router.websocket("/single")
+# async def singe_player(
+#         websocket: WebSocket,
+#         player_id: str = Query(..., alias="user_id", description="ID of the player"),
+#         competition_id: int = Query(..., alias="competition_id", description="ID of the competition")
+# ):
+#     if player_id in single_player_maker.manager.active_connections.keys():
+#         await websocket.accept()
+#         await websocket.close()
+#         return
+#     await websocket.accept()
+#     await websocket.send_json({"type": "connected", "msg": f"Игрок {player_id} подключен к серверу"})
+#     await single_player_maker.add_player(websocket, player_id, competition_id)
