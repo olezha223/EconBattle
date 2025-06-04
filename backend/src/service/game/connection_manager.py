d1cb3fdcff5788fab1b986bdc6cc2e525477a2b0
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-05 02:22:54 +0300

    добавил доку в менеджер соединений

--- backend/src/service/game/connection_manager.py
+++ backend/src/service/game/connection_manager.py
@@ -6,11 +6,14 @@ class ConnectionManager:
         self.active_connections: dict[int, WebSocket] = dict()
 
     def add_connection(self, player_id: int, websocket: WebSocket):
+        """Добавить соединение в активные"""
         self.active_connections[player_id] = websocket
 
     def remove_connection(self, player_id: int):
+        """Если еще не удалено, то удалить из активных соединений"""
         if player_id in self.active_connections.keys():
             del self.active_connections[player_id]
 
     def get_connection(self, player_id: int) -> WebSocket:
+        """Получить вебсокет игрока"""
         return self.active_connections.get(player_id)
