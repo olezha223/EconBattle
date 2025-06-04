980fbeed2a4950bc976c33aca24e64c9049b0eeb
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-04 16:45:41 +0300

    бэм бэм улучшаем фронт кнопочки красим

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from 'react-router-dom'
 import styles from './LoginPage.module.css'
+import GoogleIcon from './GoogleIcon'
 
 export default function LoginPage() {
   const [searchParams] = useSearchParams()
@@ -11,9 +12,22 @@ export default function LoginPage() {
 
   return (
     <div className={styles.container}>
-      <button onClick={handleLogin} className={styles.loginButton}>
-        Login with Google
-      </button>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Добро пожаловать!</h1>
+        <p className={styles.description}>
+          Это приложение для преподавателей и студентов для отработки навыков решения задач по экономическим моделям!
+        </p>
+
+        <button onClick={handleLogin} className={styles.loginButton}>
+          <GoogleIcon className={styles.googleIcon} />
+          <span>Войти через Google</span>
+        </button>
+
+        <p className={styles.contact}>
+          По всем вопросам можно написать на почту <br />
+          <a href="mailto:oashvetsov@edu.hse.ru">oashvetsov@edu.hse.ru</a>
+        </p>
+      </div>
     </div>
   )
 }
