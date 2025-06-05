0b90da6ec0255421ac71d8bcf499298d2f3ab08c
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-05 15:20:58 +0300

    правим путь для страницы ошибки

--- frontend/src/pages/AuthSuccessPage/AuthSuccessPage.jsx
+++ frontend/src/pages/AuthSuccessPage/AuthSuccessPage.jsx
@@ -5,6 +5,7 @@ export default function AuthSuccessPage({ setIsAuthenticated }) {
   const navigate = useNavigate()
   const [searchParams] = useSearchParams()
 
+  // ставим данные пользователя из гугла в локальное хранилище
   useEffect(() => {
     const sub = searchParams.get('sub')
     const name = searchParams.get('name')
@@ -16,7 +17,8 @@ export default function AuthSuccessPage({ setIsAuthenticated }) {
       setIsAuthenticated(true)
       navigate(redirect)
     } else {
-      navigate('/?error=auth_failed')
+      // если что-то не то, сразу на повторную авторизацию редирект
+      navigate('/login-error')
     }
   }, [navigate, searchParams, setIsAuthenticated])
 
