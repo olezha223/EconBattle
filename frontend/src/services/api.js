4013eaa5e58b8a5d8112de0041f5997e7e3cbf0c
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-05 14:45:52 +0300

    комментарии на сервисы

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,6 +5,7 @@ const api = axios.create({
   withCredentials: true
 })
 
+// Функция для получения айди пользователя
 export const getUserId = () => localStorage.getItem('user_id')
 
 export const fetchUserTasks = async () => {
@@ -16,6 +17,7 @@ export const fetchUserTasks = async () => {
   return response.data
 }
 
+// Получить превью публичных задач, которые создал пользователь
 export const fetchPreviews = async (userId) => {
   const response = await api.get('/tasks/previews-public', {
     params: { user_id: userId },
@@ -24,6 +26,7 @@ export const fetchPreviews = async (userId) => {
   return response.data
 }
 
+// Получить превью соревнований, которые создал пользователь
 export const fetchUserCompetitions = async (userId) => {
   const response = await api.get('/competitions/previews', {
     params: { user_id: userId },
@@ -32,16 +35,19 @@ export const fetchUserCompetitions = async (userId) => {
   return response.data
 }
 
+// Получить превью всех задач из банка платформы
 export const fetchAllTasks = async () => {
   const response = await api.get('/tasks/all', {withCredentials: true })
   return response.data
 }
 
+// Получить превью всех соревнований из банка платформы
 export const fetchCompetitions = async () => {
   const response = await api.get('/competitions/all', {withCredentials: true })
   return response.data
 }
 
+// Получить детальную информацию о соревновании
 export const fetchCompetitionDetails = async (competitionId) => {
   const response = await api.get('/competitions/', {
     params: { competition_id: competitionId },
@@ -50,6 +56,7 @@ export const fetchCompetitionDetails = async (competitionId) => {
   return response.data
 }
 
+// Получить ссылку на аватарку пользователя в гугле
 export const fetchPictureUrl = async () => {
   const response = await api.get(`/users/picture`, {
     params: { user_id: getUserId() },
@@ -58,7 +65,7 @@ export const fetchPictureUrl = async () => {
   return response.data;
 };
 
-
+// Получить детальную статистику пользователя на платформе
 export const fetchUserInfo = async (userId) => {
   const response = await api.get('/users/info', {
     params: { user_id: userId },
@@ -67,6 +74,7 @@ export const fetchUserInfo = async (userId) => {
   return response.data
 }
 
+// Получить детальную информацию о задаче
 export const fetchTaskDetails = async (taskId) => {
   const response = await api.get('/tasks/', {
     params: { task_id: taskId },
@@ -75,11 +83,13 @@ export const fetchTaskDetails = async (taskId) => {
   return response.data
 }
 
+// Создать задачу
 export const createTask = async (taskData) => {
   const response = await api.post('/tasks/', taskData, {withCredentials: true });
   return response.data;
 };
 
+// Обновить имя пользователя
 export const updateUsername = async (userId, newUsername) => {
   const response = await api.put('/users/update_username', null, {
     params: {
@@ -94,6 +104,7 @@ export const updateUsername = async (userId, newUsername) => {
   return response.data
 }
 
+// Получить все задачи (в том числе и приватные) для текущего пользователя
 export const fetchUserTasksPreviews = async () => {
   const response = await api.get('/tasks/previews', {
     params: { user_id: getUserId() },
@@ -102,6 +113,7 @@ export const fetchUserTasksPreviews = async () => {
   return response.data;
 };
 
+// Создать соревнование
 export const createCompetition = async (competitionData) => {
   const response = await api.post('/competitions/', competitionData, {
     withCredentials: true
--- frontend/src/utils/timeUtils.js
+++ frontend/src/utils/timeUtils.js
@@ -1,6 +1,7 @@
 import { formatDistanceToNow } from 'date-fns'
 import { ru } from 'date-fns/locale'
 
+// Возвращает сколько времени назад произошло событие
 export function formatTimeAgo(date) {
   return formatDistanceToNow(date, {
     addSuffix: true,
